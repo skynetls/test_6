f23ede2ada4c8c670e0e312df0a99698428fe87a
skynetls <lyudocheksi@gmail.com>
2018-11-18 18:49:23 +0300

    write

--- src/main/java/task_16.java
+++ src/main/java/task_16.java
@@ -1,6 +1,30 @@
-public class task_16 {
-    public static void main(String[] args) {
-        String S;
+import java.io.*;
 
+class task_16 {
+    public static void main(String[] args) {
+        String s;
+        int i;
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите текст, по окончание ввода текста обозначте строкой 'stop'");
+
+        try (FileWriter w = new FileWriter("text.txt"))
+        {
+            do {
+                System.out.print(": ");
+                s = r.readLine();
+
+                i = s.trim().compareTo("stop");
+
+                if (i == 0)
+                    break;
+
+                s = s + "\r\n";
+                w.write(s);
+            }
+            while (i != 0);
+        }
+        catch (IOException exc) {
+            System.out.println("Ошибка ввода/вывода: " + exc);
+        }
     }
 }
