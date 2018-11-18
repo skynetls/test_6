b32bc2efa83fa0ccb8f0ad12841e0d8aab9d92f1
skynetls <lyudocheksi@gmail.com>
2018-11-18 22:41:45 +0300

    correct

--- src/main/java/task_16.java
+++ src/main/java/task_16.java
@@ -3,7 +3,6 @@ import java.io.*;
 class task_16 {
     public static void main(String[] args) {
         String s;
-        int i;
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите текст, по окончание ввода текста обозначте строкой 'stop'");
 
@@ -12,15 +11,13 @@ class task_16 {
                 System.out.print(": ");
                 s = r.readLine();
 
-                i = s.trim().compareTo("stop");
-
-                if (i == 0)
+                if (s.trim().compareTo("stop") == 0)
                     break;
 
                 s = s + "\r\n";
                 w.write(s);
             }
-            while (i != 0);
+            while (s.trim().compareTo("stop") != 0);
         }
         catch (IOException exc) {
             System.out.println("Ошибка ввода/вывода: " + exc);
